35c4be5ba52b8ddf51ec25bbb1df0bb4cb7b034f
makehoney <buffalo1332@gmail.com>
2017-12-02 14:31:49 +0900

    clear_main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-/*
-        Shapes2D abc = new Shapes2D.Square(0, 0, 5, 3);
-        Shapes2D bbb = new Shapes2D.EquilateralTriangle(2, 0, 0, 0, 1, 1.7315);
-        Shapes2D ccc = new Shapes2D.Circle(0, 0, 3, 3);
-        Shapes3DPrism ddd = new Cone(0, 0, 0, 3, 4.243);  // 참조변수 형이 shapes3DPyramid면 getNHeight불가능
-        Shapes3DPrism eee = new EquilateralTriangularPyramid(2, 0, 0, 0, 1,
-                1.7315, 2.0);
-        Shapes3DPrism fff = new SquarePyramid(0, 4, 4, 0, 2.8);
-        Shapes3DPrism ggg = new Cylinder(2, 3, 3, 4, 10);
-        Shapes3DPrism hhh = new EquilateralTriangularPrism(2, 0, 0, 0, 1,
-                1.7315, 2.0);
-        Shapes3DPrism iii = new SquarePrism(0, 4, 4, 0, 2);
-        System.out.println(ggg.toString());
-*/
         Manager.chooseShape();
     }
 }
